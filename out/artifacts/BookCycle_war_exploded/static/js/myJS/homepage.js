b3e4a242b4b57561f963a9c2f45b52e0ef2ba040
吴超民 <472171582>
2021-04-15 11:37:09 +0800

    第五次提交，添加了前端user，manager文件

---
+++ out/artifacts/BookCycle_war_exploded/static/js/myJS/homepage.js
@@ -0,0 +1,15 @@
+
+// function myInformation() {
+//     $('#homepage').html("");
+//     $('#homepage').load("information.html");
+//     // var str=document.getElementById("information").innerHTML;
+//     // $('#homepage').html(str);        
+// }
+/* <script type="text/html" id="information">
+    <div style="width: 100%; height: 49px;padding-top:0px; border-bottom: 1px solid #dddddd;">
+        <div style="width: 5px;height: 20px;float: left; margin-left: 30px; margin-top: 15px; border-radius: 2px;background-color: rgb(63, 164, 247);"></div>
+        <span style="float: left; color: rgb(63, 164, 247);margin-top: 15px;margin-left: 5px;">我的信息</span>
+    </div> 
+</script> */
+
+  
